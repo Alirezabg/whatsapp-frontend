8679da58be675dbeefe9d8ba1f0fd6f221330c99
Ali Bagheri <23119@stmarys.ac.uk>
2024-04-08 17:12:57 +0100

    making homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,163 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import UserList from './components/UserList';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import cors from 'cors';
+export const fetchUsers = async () => {
+  try {
+    const response = await axios.get(
+      `https://whatsapp-poc-api-function.azurewebsites.net/api/users`,
+      { withCredentials: true },
+    );
+    console.log('response', response);
+    return response; // This will include the response data, status, and other information
+  } catch (error) {
+    // Handle or throw the error as needed
+    console.error('Error fetching users:', error);
+    throw error;
+  }
+};
+export const fetchMessages = async (userId) => {
+  try {
+    const response = await axios.get(
+      `https://whatsapp-poc-api-function.azurewebsites.net/api/Messages?UserId=${userId}`,
+      { withCredentials: true },
+    );
+    console.log('response', response);
+    return response; // This will include the response data, status, and other information
+  } catch (error) {
+    // Handle or throw the error as needed
+    console.error('Error fetching users:', error);
+    throw error;
+  }
+};
+export const getUsers = async () => {
+  try {
+    const response = await fetchUsers();
+    return response.data;
+  } catch (error) {
+    // Handle error...
+    console.error('Error fetching users:', error);
+    return [];
+  }
+};
 
 function App() {
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
   const [activeUserCode, setActiveUserCode] = useState('');
   const [users, setUsers] = useState([]);
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
-    fetch('https://whatsapp-poc-api-function.azurewebsites.net/api/users', {
-      mode: 'no-cors',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(
-            `Network response was not ok, status: ${response.status}`,
-          );
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Received data:', data);
-        setUsers(data.id);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+    const fetchUsersData = async () => {
+      const usersData = await getUsers();
+      setUsers(usersData);
+    };
+
+    fetchUsersData();
   }, []);
+  useEffect(() => {
+    const fetchMessagesData = async () => {
+      const response = await fetchMessages(activeUserCode);
+      const messagesData = response.data;
+      setMessages(messagesData);
+      console.log('messagesData', messagesData);
+    };
+
+    fetchMessagesData();
+  }, []);
+  const handleSort = (key) => {
+    if (sortBy === key) {
+      // If already sorted by the same key, toggle the sort order
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      // If sorting by a different key, set the new key and default to ascending order
+      setSortBy(key);
+      setSortOrder('asc');
+    }
+  };
+  const compareValues = (key, order = 'asc') => {
+    return function (a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // Property doesn't exist on either object
+        return 0;
+      }
+
+      const varA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key];
+      const varB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+
+      return order === 'desc' ? comparison * -1 : comparison;
+    };
+  };
   return (
     <>
       <Router>
         <Navbar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/news" element={<About />} />
+            {/* <Route path="/" element={<About />} />
+            <Route path="/news" element={<About />} /> */}
             {/* Define other routes that you need*/}
           </Routes>
+          <div className="container">
+            <div className="left">
+              <div>
+                <h1>Users</h1>
+                <ul>
+                  {users.map((user) => (
+                    <li
+                      key={user.id}
+                      onClick={(e) => setActiveUserCode(user.id)}>
+                      {user.display_name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="right">
+              <div>
+                <h1>Active User</h1>
+                <h2>active user : {activeUserCode}</h2>
+                <table>
+                  <thead>
+                    <tr>
+                      <th onClick={() => handleSort('timestamp')}>
+                        Timestamp{' '}
+                        {sortBy === 'timestamp' &&
+                          (sortOrder === 'asc' ? '▲' : '▼')}
+                      </th>
+                      <th onClick={() => handleSort('message_body')}>
+                        Message{' '}
+                        {sortBy === 'message_body' &&
+                          (sortOrder === 'asc' ? '▲' : '▼')}
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {messages
+                      .filter((message) => message.user_id === activeUserCode)
+                      .sort(compareValues(sortBy, sortOrder))
+                      .map((message) => (
+                        <tr key={message.id}>
+                          <td>
+                            {new Date(message.timestamp).toLocaleString()}
+                          </td>
+                          <td>{message.message_body}</td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </main>
-        <div>
-          <h1>Users</h1>
-          <ul>
-            {users.map((user) => (
-              <li key={user.id} onClick={(e) => setActiveUserCode(user.id)}>
-                {user.display_name}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h1>Active User</h1>
-          <p>{activeUserCode}</p>
-        </div>
       </Router>
       <footer className="footer">
         <p>&copy; 2024</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,39 +33,10 @@ const Navbar = () => {
             </li>
             <li className="nav__item">
               <NavLink
-                to="/news"
-                className="nav__link"
-                onClick={closeMenuOnMobile}>
-                News
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink
                 to="/about-us"
                 className="nav__link"
                 onClick={closeMenuOnMobile}>
                 About Us
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink
-                to="/favorite"
-                className="nav__link"
-                onClick={closeMenuOnMobile}>
-                Favorite
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink
-                to="/location"
-                className="nav__link"
-                onClick={closeMenuOnMobile}>
-                Location
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/get-started" className="nav__link nav__cta">
-                Get Started
               </NavLink>
             </li>
           </ul>
